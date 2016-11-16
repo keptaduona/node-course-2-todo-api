4670385c4d241e79eefbec6750ff4b6319abea3f
Matas Kinderis <mataskind@gmail.com>
2016-11-16 23:49:26 +0200

    Add DELETE /todos/:id route

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 // connect to MongoDB and add ES6 global Promises to mongoose
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
