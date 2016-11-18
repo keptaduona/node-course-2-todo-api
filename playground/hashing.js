c2e9513e414a63bcbc7af34a7d9ff6e968a3f251
Matas Kinderis <mataskind@gmail.com>
2016-11-18 03:14:10 +0200

    Add post /users route and token method(auth)

---
+++ playground/hashing.js
@@ -0,0 +1,38 @@
+// npm install jsonwebtoken
+const {SHA256} = require('crypto-js'); //just playground
+// var message = 'begemotas';
+// var hash = SHA256(message).toString();
+//
+// console.log(`Message: ${message}`);
+// console.log(`Hash: ${hash}`);
+//
+// var data = {
+//   id: 4
+// };
+// var token = {
+//   data,
+//   hash: SHA256(JSON.stringify(data) + 'somesecret').toString()
+// };
+//
+// // token.data.id = 5;
+// // token.hash = SHA256(JSON.stringify(token.data)).toString();
+// // JSON WEB TOKEN
+// var resultHash = SHA256(JSON.stringify(token.data) + 'somesecret').toString();
+// if (resultHash === token.hash) {
+//   console.log('Data was not changed');
+// } else {
+//   console.log('Data was changed. Do not trust!');
+// }
+
+// RAEL SHIT STARTS NOW
+const jwt = require('jsonwebtoken');
+
+var data = {
+  id: 10
+};
+
+var token = jwt.sign(data, '123abc'); // create the token for user
+console.log(token);
+
+var decoded = jwt.verify(token, '123abc');
+console.log('decoded', decoded);
