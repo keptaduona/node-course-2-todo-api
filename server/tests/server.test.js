6c831867e209409bfb435ea7f125b433de71df35
Matas Kinderis <mataskind@gmail.com>
2016-11-21 12:38:19 +0200

    Add tests fir DELETE /users/me/token

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -297,7 +297,27 @@ describe('POST /users/login', () => {
           done();
 //if we dont catch and error occurs, test will time out
         }).catch((e) => done(e));
+      });
+    });
+});
+
+describe('DELETE /users/me/token', () => {
+  it('should remove auth token on logout', (done) => {
+    request(app)
+      .delete('/users/me/token')
+      .set('x-auth', users[0].tokens[0].token)
+      .expect(200)
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        User.findById(users[0]._id).then((user) => {
+          expect(user.tokens.length).toBe(0);
+          done();
+        }).catch((e) => done(e));
+      })
   });
 
-});
+
 });
